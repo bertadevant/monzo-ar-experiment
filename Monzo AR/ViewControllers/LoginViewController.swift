94787b2374848eb9b8209f946dc94bdf28e48ff5
Wayne Rumble <rumblewayne@gmail.com>
2018-02-04 11:27:45 +0000

    Add launchscreen and setup VC

---
+++ Monzo AR/ViewControllers/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Monzo AR
+//
+//  Created by Niamh Power on 29/01/2018.
+//  Copyright © 2018 Novoda. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
